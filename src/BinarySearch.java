70c4b0632175f189d24aa5def830b2c2d77dafd0
Dmitri Mandaji <badmopss@gmail.com>
2023-04-06 16:50:27 +0200

    add class BinarySearch

---
+++ src/BinarySearch.java
@@ -0,0 +1,13 @@
+
+
+public class BinarySearch {
+  public static int BinarySearch(int[] num, int target) {
+    int left = 0;
+    int right = 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+
+
+    }
+  }
+}
